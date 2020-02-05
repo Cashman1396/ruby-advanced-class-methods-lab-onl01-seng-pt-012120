88634502bd2a88d52f7acffff64e7844d96e7cb5
Chris Ashman <chrisashman96@gmail.com>
2020-02-05 23:49:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,22 @@ class Song
     song.save
     song 
   end
+  
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song 
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.create 
+    song.name = song_name
+    song 
+  end 
+  
+  def self.find_by_name(song_name)
+    self.all.detect{|s| s.name == song_name 
+  end
 
 
 
@@ -25,8 +41,7 @@ class Song
 
 
 
-
-
+ 
 
 
 
